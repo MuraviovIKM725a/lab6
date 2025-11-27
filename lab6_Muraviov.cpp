f26f482e428f904284bde7335acc3b6aceab1998
MuraviovIKM725a <Andrii.Muraviov@infiz.khpi.edu.ua>
2025-11-27 09:16:42 +0100

    Add files via upload

---
+++ lab6_Muraviov.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "ukr");
+    int a, b;
+
+    cout << "Введіть два натуральних числа: ";
+    cin >> a >> b;
+
+    if (a <= 0 || b <= 0) {
+        cout << "Числа повинні бути натуральними" << endl;
+        return 0;
+    }
+
+    int temp;
+    do {
+        if (a > b)
+            a = a % b;
+        else
+            b = b % a;
+    } while (a != 0 && b != 0);
+
+    int gcd = (a == 0 ? b : a);
+
+    cout << "НСД = " << gcd << endl;
+
+    return 0;
+}
